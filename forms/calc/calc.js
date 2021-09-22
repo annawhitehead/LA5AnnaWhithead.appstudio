7df4592164a3c9c5b5417eb4e78a7ed0ffb4806e
annawhitehead <annawhitehead00@gmail.com>
2021-09-22 14:51:00 -0500

    Initial commit

---
+++ forms/calc/calc.js
@@ -0,0 +1,9 @@
+function calculate(num1, num2){
+  let functionSum= num1+num2
+  return functionSum
+  }
+  let num1= Number(prompt("Please enter the first number."));
+  let num2=Number(prompt("Please enter the second number."));
+   
+let summedNum = calculate(num1,num2) 
+console.log(`The sum of ${num1} and ${num2} is ${summedNum}`)
